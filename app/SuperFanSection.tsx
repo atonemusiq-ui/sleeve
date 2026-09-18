b168c12636abb05fe9257d4867ed6dc87de9f9f1
atonemusiq-ui <atonemusiq@gmail.com>
2026-09-18 06:02:59 -0700

    Fix formatting in SuperFanSecFix build: close missing referral section divtion component

--- app/SuperFanSection.tsx
+++ app/SuperFanSection.tsx
@@ -120,6 +120,7 @@ export default function SuperFanSection({
                                           {copied ? "Copied!" : "Copy"}
                                         </button>                    
                   </div>
+                  </div>
               )}
         </section>
       );
